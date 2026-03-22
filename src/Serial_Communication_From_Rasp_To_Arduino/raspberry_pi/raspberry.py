b6c84359993882516d93818351696f44f38f475b
quan <quan.dhww@gmail.com>
2026-03-22 11:17:33 +0700

    add code governing checking that if arduino is still running loop function

--- src/Serial_Communication_From_Rasp_To_Arduino/raspberry_pi/raspberry.py
+++ src/Serial_Communication_From_Rasp_To_Arduino/raspberry_pi/raspberry.py
@@ -2,22 +2,51 @@ import serial
 import time
 
 if __name__ == "__main__":
-    ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
+    ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
     ser.reset_input_buffer()
     number = 0
 
-    """Opening serial port from rasp causes arduino connected by USB cable to reset"""
     while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode("utf-8").rstrip()
-            if line == "ready!":
-                break
+        """Opening serial port from rasp causes arduino connected by USB cable to reset"""
+        # --- Handshake Phase ---
+        print("Waiting for Arduino to calibrate...")        
+        while True:
+            if ser.in_waiting > 0:
+                try:
+                    line = ser.readline().decode("utf-8").strip()
+                    if line == "ready!":
+                        print("Connection established! Robot is balanced.")
+                        break
+                except:
+                    continue
 
-    while True:
-        left_motor_speed = int(input("Enter left motor speed: "))
-        right_motor_speed = int(input("Enter right motor speed: "))
-        direction = int(input("Enter direction: "))
-        if direction != 0 and direction != 1:
-            direction = 0
-        ser.write(bytearray([left_motor_speed, right_motor_speed, direction]))
-        time.sleep(1)
+        while True:
+            left_motor_speed = int(input("Enter left motor speed: "))
+            right_motor_speed = int(input("Enter right motor speed: "))
+            direction = int(input("Enter direction: "))
+            if direction != 0 and direction != 1:
+                direction = 0
+            ser.write(bytearray([left_motor_speed, right_motor_speed, direction]))
+            time.sleep(1)
+            start_checking_tick_time = (int)(time.time())
+            acknowledged = False
+            while True:
+                if ser.in_waiting > 0:
+                    try:
+                        line = ser.readline().decode("utf-8").strip()
+                        print(line)
+                        if line == "arduino_ack_tick":
+                            acknowledged = True
+                            break
+                    except:
+                        continue
+
+                if (int)(time.time()) - start_checking_tick_time > 1:
+                        print("No acknowledged tick from arduino, try handshaking again.")
+                        acknowledged = False
+                        break
+            
+            if acknowledged == False:
+                break
+                
+                
